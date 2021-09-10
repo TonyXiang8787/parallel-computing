c8a91b4958b54ab2c187a0b18dd3696ba006856d
Tony Xiang <tonyxiang@live.nl>
2021-09-10 08:27:34 +0200

    Update conda-windows.ps1

--- .github/conda-windows.ps1
+++ .github/conda-windows.ps1
@@ -1,6 +1,7 @@
 if (-not (Test-Path C:\conda_envs -PathType Container)) 
 {
-  "$env:CONDA\bin\conda" create --yes -p C:\conda_envs\cpp_pkgs -c conda-forge python=3.9
-  "$env:CONDA\bin\conda" activate cpp_pkgs
+  C:\Miniconda\condabin\conda init
+  conda create --yes -p C:\conda_envs\cpp_pkgs -c conda-forge python=3.9
+  conda activate C:\conda_envs\cpp_pkgs
   conda install --yes -c conda-forge boost-cpp eigen nlohmann_json mkl mkl-devel
 }
