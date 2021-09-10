ce92cadc0d63c51e10d274ec98cd5a7bf2bd4be2
Tony Xiang <tonyxiang@live.nl>
2021-09-10 08:49:01 +0200

    Update conda-windows.ps1

--- .github/conda-windows.ps1
+++ .github/conda-windows.ps1
@@ -7,15 +7,14 @@ if (-not (Test-Path C:\conda_envs -PathType Container))
   conda create --yes -p C:\conda_envs\cp37-cp37m -c conda-forge python=3.7
   conda activate C:\conda_envs\cp37-cp37m
   pip install --no-cache-dir -r dev-requirements.txt
-  pip install --no-cache-dir twine
   
   conda create --yes -p C:\conda_envs\cp38-cp38 -c conda-forge python=3.8
   conda activate C:\conda_envs\cp38-cp38
   pip install --no-cache-dir -r dev-requirements.txt
-  pip install --no-cache-dir twine
   
   conda create --yes -p C:\conda_envs\cp39-cp39 -c conda-forge python=3.9
   conda activate C:\conda_envs\cp39-cp39
   pip install --no-cache-dir -r dev-requirements.txt
-  pip install --no-cache-dir twine
+  
+  conda clean --yes --all
 }
